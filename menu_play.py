b60edf1b078919ebab0e82907959fd1b235f8c52
BryanManzano2016 <bryanmanzano2@gmail.com>
2019-03-25 20:05:37 -0500

    Interfaz grafica 100%

---
+++ menu_play.py
@@ -0,0 +1,171 @@
+import pygame
+from src import Config
+from src.Game2 import *
+from src.Game import *
+
+class Menu_play:
+
+    def __init__(self, display, modo):
+
+        self.display = display
+        self.width_total = Config['game']['width']
+        self.width_able = Config['game']['width'] - Config['game']['bumper_size']*15
+        self.height_total = Config['game']['height']
+        self.height_able = Config['game']['height'] - Config['game']['bumper_size'] * 1
+        self.square_size = Config['game']['square_size']
+        self.height_button = Config['game']['height_button']
+        self.width_button = Config['game']['width_button']
+        self.modo = modo
+
+        print(modo)
+
+        self.menu_main()
+
+    def menu_main(self):
+
+        clock = pygame.time.Clock()
+
+        while True:
+
+            events = pygame.event.get()
+
+            for event in events:
+                # Si se sale del programa
+                if event.type == pygame.QUIT:
+                    exit()
+
+                # Si se presiona una tecla
+                if event.type == pygame.KEYDOWN:
+                    pass
+
+                # posiciones de mouse y clicks
+                mouse = pygame.mouse.get_pos()
+                click = pygame.mouse.get_pressed()
+
+                if self.modo == "invit":
+
+                    self.buttons_click_i((self.width_total / 2) - (self.width_button / 2),
+                                  (self.height_total / 4) - (self.height_button / 1.5),
+                                  self.width_button,
+                                  50,
+                                  mouse,
+                                  click)
+
+                elif self.modo == "multi":
+
+                    self.buttons_click_i((self.width_total / 2) - (self.width_button / 2),
+                                  (self.height_total / 4) - (self.height_button / 1.5),
+                                  self.width_button,
+                                  50,
+                                  mouse,
+                                  click)
+
+                    self.buttons_click((self.width_total / 2) - (self.width_button / 2),
+                        (self.height_total / 2) - self.height_button + 20,
+                        self.width_button,
+                        50,
+                        mouse,
+                        click)
+
+            # Fill background and draw game area
+            self.display.fill(Config['colors']['green'])
+
+            # Rectangulo blanco para play
+            pygame.draw.rect(
+                self.display,
+                Config['colors']['white'],
+                [
+                    (self.width_total / 2) - self.width_button,
+                    (self.height_total / 4) - self.height_button,
+                    self.width_button * 2,
+                    self.height_button
+                ]
+            )
+
+            # Rectangulo para boton play
+            pygame.draw.rect(
+                self.display,
+                Config['colors']['blue'],
+                [
+                    (self.width_total / 2) - (self.width_button / 2),
+                    (self.height_total / 4) - (self.height_button / 1.5),
+                    self.width_button,
+                    50
+                ]
+            )
+
+            # Initialize font and draw
+            pygame.font.init()
+            font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
+
+            # textos
+            play_text = font.render("PLAY", False, Config['colors']['white'])
+
+            play_text_rect = play_text.get_rect(
+                center=(
+                    (self.width_total / 2) + 10,
+                    (self.height_total / 4) - 40,
+                )
+            )
+
+            if self.modo == "multi":
+                # Rectangulo blanco para multiplayer
+                pygame.draw.rect(
+                    self.display,
+                    Config['colors']['white'],
+                    [
+                        (self.width_total / 2) - self.width_button,
+                        (self.height_total / 2) - self.height_button,
+                        self.width_button * 2,
+                        self.height_button
+                    ]
+                )
+
+                # Rectangulo azul para boton multiplayer
+                pygame.draw.rect(
+                    self.display,
+                    Config['colors']['blue'],
+                    [
+                        (self.width_total / 2) - (self.width_button / 2),
+                        (self.height_total / 2) - self.height_button + 20,
+                        self.width_button,
+                        50
+                    ]
+                )
+
+                multiplayer_text = font.render('MULTIPLAYER', False, Config['colors']['white'])
+
+                multiplayer_text_rect = multiplayer_text.get_rect(
+                    center=(
+                        (self.width_total / 2) + 15,
+                        (self.height_total / 2) - self.height_button + 40
+                    )
+                )
+
+                self.display.blit(multiplayer_text, multiplayer_text_rect)
+
+            self.display.blit(play_text, play_text_rect)
+
+            pygame.display.update()
+            clock.tick(Config['game']['fps'])
+
+
+    def buttons_click(self, x, y, wd, hg, mouse, click):
+
+        if x + wd > mouse[0] > x and y + hg > mouse[1] > y:
+
+            if click[0] == 1:
+                game = Game(self.display)
+
+    def buttons_click_i(self, x, y, wd, hg, mouse, click):
+
+        if x + wd > mouse[0] > x and y + hg > mouse[1] > y:
+
+            if click[0] == 1:
+                game = Game2(self.display)
+
+def function_inv():
+    print("inv")
+
+def function_log(data):
+    print(data)
---
+++ src/Game2.py
@@ -0,0 +1,152 @@
+import pygame
+from src.Player import Player
+from src.PointWin import PointWin
+from src.Config import Config
+import socket
+import json
+
+class Game2:
+
+    def __init__(self, display):
+
+        self.display = display
+        self.width_total = Config['game']['width']
+        self.width_able = Config['game']['width'] - Config['game']['bumper_size']*15
+        self.height_total = Config['game']['height']
+        self.height_able = Config['game']['height'] - Config['game']['bumper_size'] * 1
+        self.square_size = Config['game']['square_size']
+
+        self.score = 0
+
+        self.loop()
+
+    def loop(self):
+
+        clock = pygame.time.Clock()
+
+        positions_free, positions, pos_m = lists()
+
+        #Creacion y ubicacion de jugadores y meta
+        posP = pos_m[0]
+        player_1 = Player(self.display, posP)
+
+        players = list()
+        players.append(player_1)
+
+        posW = pos_m[2]
+        pointWin = PointWin(self.display, posW)
+
+        while True:
+
+            pos_change = [[0, 0], [0, 0]]
+
+            for event in pygame.event.get():
+                # Si se sale del programa
+                if event.type == pygame.QUIT:
+                    exit()
+
+                # Si se presiona una tecla
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_LEFT:
+                        pos_change[0][0] += -self.square_size
+                        pos_change[0][1] = 0
+
+                    elif event.key == pygame.K_RIGHT:
+                        pos_change[0][0] += self.square_size
+                        pos_change[0][1] = 0
+
+                    elif event.key == pygame.K_UP:
+                        pos_change[0][0] = 0
+                        pos_change[0][1] += -self.square_size
+
+                    elif event.key == pygame.K_DOWN:
+                        pos_change[0][0] = 0
+                        pos_change[0][1] += self.square_size
+
+            # Fill background and draw game area
+            self.display.fill(Config['colors']['green'])
+
+            # Rectangulo blanco de juego
+            pygame.draw.rect(
+                self.display,
+                Config['colors']['white'],
+                [
+                    self.square_size,
+                    self.square_size,
+                    self.width_able,
+                    self.height_able
+                ]
+            )
+
+            for x in positions:
+                pygame.draw.rect(
+                    self.display,
+                    Config['colors']['black'],
+                    [
+                        x[0],
+                        x[1],
+                        self.square_size,
+                        self.square_size
+                    ]
+                )
+
+            for playerN in range(1):
+
+                if (players[playerN].movimientoValido(pos_change[playerN], positions) or not
+                        players[playerN].movimientoValido(pos_change[playerN], positions_free)):
+                    continue
+
+                players[playerN].move(pos_change[playerN])
+
+                if [players[playerN].get_posx(), players[playerN].get_posy()] == posW:
+                    self.score += 1
+                    self.loop()
+
+                players[playerN].draw()
+
+            pointWin.draw()
+
+            # Initialize font and draw title and score text
+            pygame.font.init()
+
+            font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
+
+            score_text = 'Puntuacion: {}'.format(self.score)
+            score = font.render(score_text, False, Config['colors']['white'])
+            title = font.render('LABERINTO', False, Config['colors']['white'])
+
+            title_rect = title.get_rect(
+                center=(
+                    self.width_able + (self.width_total - self.width_able)/2,
+                    100
+                )
+            )
+
+            score_rect = score.get_rect(
+                center=(
+                    self.width_able + (self.width_total - self.width_able)/2,
+                    200
+                )
+            )
+
+            self.display.blit(score, score_rect)
+            self.display.blit(title, title_rect)
+
+            pygame.display.update()
+            clock.tick(Config['game']['fps'])
+
+def lists():
+    HOST = '127.0.0.1'
+    PORT = 60000
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        data_all = ""
+        while True:
+            data = sock.recv(4096).decode()
+            if not data:
+                break
+            data_all += data
+        from_js = json.loads(data_all)
+    sock.close()
+    return from_js["positions_free"], from_js["positions"], from_js["pos"]
